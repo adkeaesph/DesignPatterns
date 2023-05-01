70b17db9d6e587e25cbc8af8af67913e4798624b
Akash Deep <adkeaesph720@gmail.com>
2023-05-01 07:35:00 +0530

    driver class committed

--- src/main/java/com/demo/designpatterns/Driver.java
+++ src/main/java/com/demo/designpatterns/Driver.java
@@ -1,7 +1,16 @@
 package com.demo.designpatterns;
 
+import com.demo.designpatterns.singleton.SingletonByEI;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("hello");
+        SingletonByEI singletonInstance1 = SingletonByEI.getInstance();
+        SingletonByEI singletonInstance2 = SingletonByEI.getInstance();
+        if(singletonInstance1.equals(singletonInstance2))
+            System.out.println("Singleton instances are the same object. " +
+                "This is known during compile-time only");
+        else {
+            System.out.println("unreachable code");
+        }
     }
 }
